e288e263180cc3b6f50bc02a5fd4973be271201b
Sarah Kaiser <70863473+sarah-kaiser@users.noreply.github.com>
2023-02-01 14:46:42 +0000

    Now with title, done for now

--- script_SelectSpecificCard.js
+++ script_SelectSpecificCard.js
@@ -23,6 +23,7 @@ fetch("backgrounds.json")
     }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
     animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.2s backwards;" >
         <h1> ${backgrounds.Origin[cardNum].Title}</h1>
+        <h2>Origin</h2>
         <p> ${backgrounds.Origin[cardNum].Content}</p>
         <hr>
         <ul>${questionList(backgrounds.Origin[cardNum].Questions)}</ul>
@@ -33,6 +34,7 @@ fetch("backgrounds.json")
     }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
     animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;">
         <h1> ${backgrounds.Twist[cardNum1].Title}</h1>
+        <h2>Twist</h2>
         <p> ${backgrounds.Twist[cardNum1].Content}</p>
         <hr>
         <ul>${questionList(backgrounds.Twist[cardNum1].Questions)}</ul>
@@ -42,6 +44,7 @@ fetch("backgrounds.json")
     }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
     animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 1s backwards;" >
         <h1> ${backgrounds.Motivation[cardNum2].Title}</h1>
+        <h2>Motivation</h2>
         <p> ${backgrounds.Motivation[cardNum2].Content}</p>
         <hr>
         <ul>${questionList(backgrounds.Motivation[cardNum2].Questions)}</ul>
@@ -51,6 +54,7 @@ fetch("backgrounds.json")
     }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
     animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 1.4s backwards;" >
         <h1> ${backgrounds.Complication[cardNum3].Title}</h1>
+        <h2>Complication</h2>
         <p> ${backgrounds.Complication[cardNum3].Content}</p>
         <hr>
         <ul>${questionList(backgrounds.Complication[cardNum3].Questions)}</ul>
