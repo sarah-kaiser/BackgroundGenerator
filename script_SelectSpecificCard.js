b08998c2fb06ad8ab11546b59a3597e2e08af25e
Sarah Kaiser <70863473+sarah-kaiser@users.noreply.github.com>
2023-02-01 15:13:13 +0000

    Now with reload button!

--- script_SelectSpecificCard.js
+++ script_SelectSpecificCard.js
@@ -62,8 +62,55 @@ fetch("backgrounds.json")
     `;
   });
 
-function pickACard(bgType) {
-  //Roll a die
-  //Select a Card
-  //Display a card
+function pickACard() {
+  fetch("backgrounds.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (backgrounds) {
+      let placeholder = document.querySelector("#container");
+      placeholder.innerHTML = `
+    <div class="card" style="background-image: url(img/Paint_${
+      backgrounds.Origin[cardNum].Color
+    }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
+    animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.2s backwards;" >
+        <h1> ${backgrounds.Origin[cardNum].Title}</h1>
+        <h2>Origin</h2>
+        <p> ${backgrounds.Origin[cardNum].Content}</p>
+        <hr>
+        <ul>${questionList(backgrounds.Origin[cardNum].Questions)}</ul>
+        
+    </div>
+    <div class="card" style="background-image: url(img/Paint_${
+      backgrounds.Twist[cardNum1].Color
+    }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
+    animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;">
+        <h1> ${backgrounds.Twist[cardNum1].Title}</h1>
+        <h2>Twist</h2>
+        <p> ${backgrounds.Twist[cardNum1].Content}</p>
+        <hr>
+        <ul>${questionList(backgrounds.Twist[cardNum1].Questions)}</ul>
+    </div>
+    <div class="card" style="background-image: url(img/Paint_${
+      backgrounds.Motivation[cardNum2].Color
+    }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
+    animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 1s backwards;" >
+        <h1> ${backgrounds.Motivation[cardNum2].Title}</h1>
+        <h2>Motivation</h2>
+        <p> ${backgrounds.Motivation[cardNum2].Content}</p>
+        <hr>
+        <ul>${questionList(backgrounds.Motivation[cardNum2].Questions)}</ul>
+    </div>
+    <div class="card" style="background-image: url(img/Paint_${
+      backgrounds.Complication[cardNum3].Color
+    }.svg); -webkit-animation: tilt-in-top-1 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.6s backwards;
+    animation: tilt-in-top-1 0.6s cubic-bezier(0.215, 0.610, 0.355, 1.000) 1.4s backwards;" >
+        <h1> ${backgrounds.Complication[cardNum3].Title}</h1>
+        <h2>Complication</h2>
+        <p> ${backgrounds.Complication[cardNum3].Content}</p>
+        <hr>
+        <ul>${questionList(backgrounds.Complication[cardNum3].Questions)}</ul>
+    </div>
+    `;
+    });
 }
